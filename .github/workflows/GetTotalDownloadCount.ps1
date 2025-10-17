cb376d0299b391dea2f26ca9e35781d9fd31a5c8
Óscar Míguez Alonso <omiguezalonso@gmail.com>
2025-10-17 15:05:34 +0200

    Removed unnecessary output from Download Count getter script

--- .github/workflows/GetTotalDownloadCount.ps1
+++ .github/workflows/GetTotalDownloadCount.ps1
@@ -3,7 +3,6 @@ param ( [Parameter(Mandatory=$true)] [string]$RepoUrl)
 $lifetimeReleases = 0;
 $splitRepoUrl = $RepoURL -split "/"
 $releasesFetchUri = "https://api.github.com/repos/$($splitRepoUrl[3])/$($splitRepoUrl[4])/releases"
-Write-Output "Releases URI: $($releasesFetchUri)"
 Invoke-WebRequest -Uri $releasesFetchUri -RetryIntervalSec 20 -MaximumRetryCount 10 -Outfile releaseListFile
 $jsonReleaseList = Get-Content -Path releaseListFile -Raw | ConvertFrom-Json
 for (($k = 0); $k -lt $jsonReleaseList.Count; $k++)
@@ -12,7 +11,6 @@ for (($k = 0); $k -lt $jsonReleaseList.Count; $k++)
     for (($j = 0); $j -lt $release.assets.Count; $j++){
         $asset = $release.assets[$j]
         if ($asset.name.EndsWith(".zip")){
-            Write-Output "$($release.name): $($asset.name), $($asset.download_count) downloads"
             $lifetimeReleases++;
         }    
     }
