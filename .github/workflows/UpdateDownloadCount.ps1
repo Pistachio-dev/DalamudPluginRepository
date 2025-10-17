a060554731836ef6896c5722b7eb45a57fc62800
Óscar Míguez Alonso <omiguezalonso@gmail.com>
2025-10-17 14:42:48 +0200

    Added download amount tracking

---
+++ .github/workflows/UpdateDownloadCount.ps1
@@ -0,0 +1,22 @@
+param ( [Parameter(Mandatory=$true)] [string]$RepoUrl)
+
+$lifetimeReleases = 0;
+$splitRepoUrl = $RepoURL -split "/"
+$releasesFetchUri = "https://api.github.com/repos/$($splitRepoUrl[4])/$($splitRepoUrl[5])/releases"
+Write-Output "Releases URI: $($releasesFetchUri)"
+Invoke-WebRequest -Uri $releasesFetchUri -RetryIntervalSec 20 -MaximumRetryCount 10 -Outfile releaseListFile
+$jsonReleaseList = Get-Content -Path releaseListFile -Raw | ConvertFrom-Json
+for (($k = 0); $k -lt $jsonReleaseList.Count; $k++)
+{
+    $release = $jsonReleaseList[$k]
+    for (($j = 0); $j -lt $release.assets.Count; $j++){
+        $asset = $release.assets[$j]
+        if ($asset.name.EndsWith(".zip")){
+            Write-Output "$($release.name): $($asset.name), $($asset.download_count) downloads"
+            $lifetimeReleases++;
+        }    
+    }
+}
+
+return $lifetimeReleases
+
